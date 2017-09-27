1ca5d4c30a8e1f4e1aa4becd7ef067e99eddf179
Anne Sofie Molsted Wanscher <anne55x9@edu.easj.dk>
2017-09-27 19:34:09 +0200

    selectAllStudent variablen  som select alle (*) elemeneter i tabel student som ligger på database i azur skyen. databasen connection string er deklæret i vaiablen conn. Initiering af et nyt objekt fra SqlConnection klassen hvorefter  databasen tilgås med funktion Open på objektet. Derefter initieres et objekter af klasserne SqlCommand og SqlReader. endeligt cw på id og navn. Egentlig burde id ikke vises.

--- ClientStudentDBDirect/Program.cs
+++ ClientStudentDBDirect/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,6 +11,31 @@ namespace ClientStudentDBDirect
     {
         static void Main(string[] args)
         {
+            string selectAllStudent = "select * from student";
+
+            string conn = "Server = tcp:annesazure.database.windows.net,1433; Initial Catalog = EasjDBasw; Persist Security Info = False; User ID = anne55x9; Password = Easj2017; MultipleActiveResultSets = False; Encrypt = True; TrustServerCertificate = False; Connection Timeout = 30";
+
+            using (SqlConnection databaseConnection = new SqlConnection(conn))
+            {
+                databaseConnection.Open();
+                using (SqlCommand selectCommand = new SqlCommand(selectAllStudent, databaseConnection))
+                {
+                    using (SqlDataReader reader = selectCommand.ExecuteReader())
+                    {
+                        while (reader.Read())
+                        {
+                            int id = reader.GetInt32(0);
+                            string name = reader.GetString(1);
+                            
+                            
+                            Console.WriteLine(id + name);
+                        } 
+                    }
+                    
+                }
+            }
+
+            Console.ReadLine();
         }
     }
 }
